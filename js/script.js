ceca058693de577e389e946f942da81afd14f99f
Magnus T <89303506+Sandmos@users.noreply.github.com>
2022-01-26 11:42:53 +0100

    question 2-4

--- js/script.js
+++ js/script.js
@@ -12,3 +12,20 @@ const cats = [
     }
 ];
 
+// question 2 
+
+const heading = document.querySelector("h3");
+
+heading.innerHTML = "Updated Heading"
+console.dir("h3")
+//question 3
+
+heading.style.fontSize = "2em"
+
+// question 4
+
+heading.classList.add("subHeading")
+console.log(heading.className)
+
+// question 5
+
